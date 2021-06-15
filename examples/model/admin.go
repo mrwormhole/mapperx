7bc7eca6605ea3d40697f2a0634fbd091b9cd048
F. Talha Altınel <talhaaltinel@hotmail.com>
2021-06-15 23:57:43 +0100

    logo

--- examples/model/admin.go
+++ examples/model/admin.go
@@ -1,6 +1,6 @@
 package model
 
-//go:generate go run github.com/MrWormHole/mapperx github.com/MrWormHole/mapperx/examples/model.Admin github.com/MrWormHole/mapperx/examples/model.User
+//go:generate mapperx github.com/MrWormHole/mapperx/examples/model.Admin github.com/MrWormHole/mapperx/examples/model.User
 type Admin struct {
 	Name    string
 	ID      string
